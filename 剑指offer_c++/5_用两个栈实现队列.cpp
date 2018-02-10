4b126b9dca9a488d3f4c543b96b5fa922a43b46a
W.C.Z <839568703@qq.com>
2018-02-10 12:05:24 +0800

    5_用两个栈实现队列

---
+++ 剑指offer_c++/5_用两个栈实现队列.cpp
@@ -0,0 +1,28 @@
+#include <stack>
+using namespace std;
+
+class Solution {
+public:
+	void push(int node) {
+		stack1.push(node);
+	}
+
+	int pop() {
+		int a;
+		if (stack2.empty())
+		{
+			while (!stack1.empty()) {
+				a = stack1.top();
+				stack2.push(a);
+				stack1.pop();
+			}
+		}
+		a = stack2.top();
+		stack2.pop();
+		return a;
+	}
+
+private:
+	stack<int> stack1;
+	stack<int> stack2;
+};
