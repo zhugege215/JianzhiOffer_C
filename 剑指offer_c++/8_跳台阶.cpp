101f0269cf1086f5fc3593baedd03725c94aaa22
W.C.Z <839568703@qq.com>
2018-02-10 15:58:01 +0800

    8_9_跳台阶

---
+++ 剑指offer_c++/8_跳台阶.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+	int jumpFloor(int number) {
+		if (number <= 0)
+		{
+			return -1;
+		}
+		else if (number == 1)
+		{
+			return 1;
+		}
+		else if (number ==2)
+		{
+			return 2;
+		}
+		else
+		{
+			return jumpFloor(number - 1) + jumpFloor(number - 2);
+		}
+	}
+};
