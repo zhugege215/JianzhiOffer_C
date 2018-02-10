2f4830f1d7a5641d7ee9a3c22f327ed8a3e00138
W.C.Z <839568703@qq.com>
2018-02-10 15:02:14 +0800

    6_旋转数组的最小数字

---
+++ 剑指offer_c++/6_旋转数组的最小数字.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <stack>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+	int minNumberInRotateArray(vector<int> rotateArray) {
+		int size = rotateArray.size();
+		if (size == 0)
+		{
+			return 0;
+		}
+		int left = 0, right = size - 1, mid = 0;
+		while (rotateArray[left] >= rotateArray[right])
+		{
+			if (right - left == 1) {
+				mid = right;
+				break;
+			}
+			mid = left + (right - left) / 2;    //向下取整
+			if (rotateArray[left] == rotateArray[right] && rotateArray[left] == rotateArray[mid])
+			{
+				return MinOrder(rotateArray, left, right);
+			}
+			if (rotateArray[mid] >= rotateArray[left])
+			{
+				left = mid;
+			}
+			else {
+				right = mid;
+			}
+		}
+		return rotateArray[mid];
+	}
+private:
+	int MinOrder(vector<int> &rotateArray, int left, int right) {
+		int min = rotateArray[left];
+		for (size_t i = left + 1; i < right; i++)
+		{
+			if (rotateArray[i] < min)
+			{
+				min = rotateArray[i];
+			}
+		}
+		return min;
+	}
+};
+
+int main() {
+	Solution s;
+	//vector<int> num = { 0,1,2,3,4,5 };
+	//vector<int> num = { 4,5,6,7,1,2,3 };
+	vector<int> num = { 2,2,2,2,1,2 };
+
+	int result = s.minNumberInRotateArray(num);
+	cout << result << endl;
+	return 0;
+}
---
+++ 剑指offer_c++/test.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+//int main() {
+//	int a = 5, b = 3, c = 0;
+//	c = a / b;
+//	cout << c << endl;
+//	return 0;
+//}
