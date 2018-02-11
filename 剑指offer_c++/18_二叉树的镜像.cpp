312c341d6ff4b144ccaa96acf06ea1eee1df1e10
W.C.Z <839568703@qq.com>
2018-02-11 12:17:12 +0800

    18_

---
+++ 剑指offer_c++/18_二叉树的镜像.cpp
@@ -0,0 +1,22 @@
+struct TreeNode {
+	int val;
+	struct TreeNode* left;
+	struct TreeNode* right;
+	TreeNode(int x): val(x), left(nullptr), right(nullptr) {}
+};
+
+class Solution {
+public:
+	void Mirror(TreeNode* pRoot) {
+		if (pRoot == nullptr)
+		{
+			return;
+		}
+
+		TreeNode* temp = pRoot->left;
+		pRoot->left = pRoot->right;
+		pRoot->right = temp;
+		Mirror(pRoot->left);
+		Mirror(pRoot->right);
+	}
+};
