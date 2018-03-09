1ab7f2c24da1c3c11d6a81e32ec1ec629b610ef9
W.C.Z <839568703@qq.com>
2018-03-09 22:59:07 +0800

    update

--- 剑指offer_c++/1_二维数组中的查找.cpp
+++ 剑指offer_c++/1_二维数组中的查找.cpp
@@ -1,9 +1,10 @@
 #include <vector>
 using namespace std;
 
+//注意本题一定要从左下角或者右上角来开始找，因为如果是从左上角来找的话，右边和下边都比改数字大，就不知道该怎么移动了
 class Solution {
 public:
-	bool Find(vector<vector<int>> array, int target) {
+	bool Find(int target, vector<vector<int> > array) {
 		int rowCount = array.size();
 		int colCount = array[0].size();
 		int i, j;
---
+++ 剑指offer_c++/practice.cpp
@@ -0,0 +1,142 @@
+#include <vector>
+using namespace std;
+struct ListNode {
+	int val;
+	struct ListNode *next;
+	ListNode(int x): val(x), next(nullptr){}
+};
+
+struct TreeNode {
+	int val;
+	TreeNode *left, *right;
+	TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+};
+
+//1
+//class Solution {
+//public:
+//	bool Find(int target, vector<vector<int> > array) {
+//		if (!array.size())
+//		{
+//			return false;
+//		}
+//		
+//		int row = array.size();
+//		int col = array[0].size();
+//		int i, j;
+//
+//		for (i = 0, j = row - 1; i < col && j >= 0;)
+//		{
+//			if (array[j][i] == target) {
+//				return true;
+//			}
+//			if (array[j][i] > target) 
+//			{
+//				j--;
+//				continue;
+//			}
+//			if (array[j][i] < target)
+//			{
+//				i++;
+//				continue;
+//			}
+//		}
+//		return false;
+//	}
+//};
+
+//2
+//class Solution {
+//public:
+//	void replaceSpace(char* str, int length) {
+//		if (str == nullptr || length <0)
+//		{
+//			return;
+//		}
+//		int oldLength = 0, spaceLength = 0, newLength = 0, i = 0;
+//		while (str[i] != '\0')
+//		{
+//			oldLength++;
+//			if (str[i] == ' ')
+//			{
+//				spaceLength++;
+//			}
+//			i++;
+//		}
+//		newLength = oldLength + spaceLength * 2;
+//		if (newLength > length)
+//		{
+//			return;
+//		}
+//		while (oldLength >= 0 && newLength > oldLength)
+//		{
+//			if (str[oldLength] == ' ')
+//			{
+//				str[newLength--] = '0';
+//				str[newLength--] = '2';
+//				str[newLength--] = '%';
+//			}
+//			else {
+//				str[newLength--] = str[oldLength];
+//			}
+//			oldLength--;
+//		}
+//
+//	}
+//};
+
+//3
+//class Solution {
+//public:
+//	vector<int> list_reverse_recursion(ListNode* head) {
+//		vector<int> res;
+//		if (head != nullptr)
+//		{
+//			res.push_back(head->val);
+//			vector<int> temp = list_reverse_recursion(head->next);
+//			if (temp.size() > 0)
+//			{
+//				res.insert(res.begin(), temp.begin(), temp.end());
+//			}
+//			
+//		}
+//		return res;
+//	}
+//
+//	vector<int> list_reverse_non_recursion(ListNode* head) {
+//		vector<int> res;
+//		while (head)
+//		{
+//			res.insert(res.begin(), head->val);
+//			head = head->next;
+//		}
+//		return res;
+//	}
+//};
+
+//4
+class Solution {
+public:
+	TreeNode * reCon(vector<int> pre, vector<int> vin) {
+		TreeNode * root = reConHelp(pre, 0, pre.size() - 1, vin, 0, vin.size() - 1);
+		return root;
+	}
+private:
+	TreeNode * reConHelp(vector<int> pre, int preStart, int preEnd, vector<int> vin, int vinStart, int vinEnd) {
+		if (preStart > preEnd || vinStart > vinEnd)
+		{
+			return nullptr;
+		}
+		TreeNode* root = new TreeNode(pre[preStart]);
+		for (size_t i = vinStart; i <= vinEnd; i++)
+		{
+			if (vin[i] == pre[preStart])
+			{
+				root->left = reConHelp(pre, preStart + 1, preStart + i - vinStart, vin, vinStart, i - 1);
+				root->right = reConHelp(pre, preStart + i - vinStart + 1, preEnd, vin, i + 1, vinEnd);
+				break;
+			}
+		}
+		return root;
+	}
+};
