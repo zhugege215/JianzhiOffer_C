e6ccc943509ae4c494d14493320f143010b6f7a0
W.C.Z <839568703@qq.com>
2018-05-20 23:14:11 +0800

    update

---
+++ 剑指offer_c++/gas-station.cpp
@@ -0,0 +1,22 @@
+#include <vector>
+using namespace std;
+
+class Solution
+{
+public:
+	int canCompleteCircuit(vector<int> &gas, vector<int> &cost)
+	{
+		int start(0), total(0), tank(0);
+		for (int i = 0; i < gas.size(); i++)
+		{
+			tank = tank + gas[i] - cost[i];
+			if (tank < 0)
+			{
+				start = i + 1;
+				total += tank;
+				tank = 0;
+			}
+		}
+		return ((total + tank) < 0) ? -1 : start;
+	}
+};
